3c043286606eb6fa419219aa260b61acf2edc05a
Mauricio Souza <mauricio.salso@hotmail.com>
2026-03-07 01:53:56 -0300

    feat: implementando o metodo CreateShoppingCart

--- ShoppingCartGrpc/Services/ShoppingCartService.cs
+++ ShoppingCartGrpc/Services/ShoppingCartService.cs
@@ -3,8 +3,10 @@ using Grpc.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ShoppingCartGrpc.Data;
+using ShoppingCartGrpc.Models;
 using ShoppingCartGrpc.Protos;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ShoppingCartGrpc.Services
@@ -35,6 +37,55 @@ namespace ShoppingCartGrpc.Services
             var shoppingCartModel = _mapper.Map<ShoppingCartModel>(shoppingCart);
             //var shoppingCartModel = new ShoppingCartModel();
             return shoppingCartModel;
+        }
+
+        public override async Task<ShoppingCartModel> CreateShoppingCart(ShoppingCartModel request, ServerCallContext context)
+        {
+            var shoppingCart = _mapper.Map<ShoppingCart>(request);
+            var isExist = await _shoppingCartDbContext.ShoppingCart
+                .AnyAsync(s => s.UserName == shoppingCart.UserName);
+
+            if (isExist)
+            {
+                _logger.LogError("Invalid UserName for ShoppingCart. UserName: {UserName}", shoppingCart.UserName);
+                throw new RpcException(new Status(StatusCode.AlreadyExists, $"Shopping with Username={request.UserName} is already exists."));
+            }
+
+            _shoppingCartDbContext.ShoppingCart.Add(shoppingCart);
+            await _shoppingCartDbContext.SaveChangesAsync();
+
+            var shoppingCartModel = _mapper.Map<ShoppingCartModel>(shoppingCart);
+            return shoppingCartModel;
+        }
+
+        public override async Task<RemoveItemIntoShoppingCartResponse> RemoveItemIntoShoppingCart(RemoveItemIntoShoppingCartRequest request, ServerCallContext context)
+        {
+            // Get sc if exist or not
+            // Check item if exist in sc or not
+            // Remove item in SC db
+
+            var shoppingCart = await _shoppingCartDbContext.ShoppingCart
+                .FirstOrDefaultAsync(s => s.UserName == request.UserName);
+
+            if (shoppingCart == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Shopping cart for user {request.UserName} not found."));
+            }
+
+            var removeCartItem = shoppingCart.Items.FirstOrDefault(i => i.ProductId == request.RemoveCartItem.ProductId);
+            if (removeCartItem == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"CartItem with ProductId {request.RemoveCartItem.ProductId} not found in shopping cart."));
+            }
+
+            shoppingCart.Items.Remove(removeCartItem);
+            var removeCount = await _shoppingCartDbContext.SaveChangesAsync();
+
+            var response = new RemoveItemIntoShoppingCartResponse
+            {
+                Success = removeCount > 0
+            };
+            return response;
         }
     }
 }
