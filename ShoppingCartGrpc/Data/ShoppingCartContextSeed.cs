d5b99841791cc128df919de4d26c3998d4dbcfab
Mauricio Souza <mauricio.salso@hotmail.com>
2026-03-07 00:55:44 -0300

    feat: injetando as dependencia e valores no banco de dados - ShoppingCartContextSeed

---
+++ ShoppingCartGrpc/Data/ShoppingCartContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using ShoppingCartGrpc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingCartGrpc.Data
+{
+    public class ShoppingCartContextSeed
+    {
+        public static void SeedAsync(ShoppingCartContext shoppingCartContext)
+        {
+            if (!shoppingCartContext.ShoppingCart.Any())
+            {
+                var shoppingCarts = new List<ShoppingCart>
+                {
+                    new ShoppingCart
+                    {
+                        UserName = "swn",
+                        Items = new List<ShoppingCartItem>
+                        {
+                            new ShoppingCartItem
+                            {
+                                Quantity = 2,
+                                Color = "Black",
+                                Price = 10.0f,
+                                ProductId = 1,
+                                ProductName = "Product 1",
+                            },
+                            new ShoppingCartItem
+                            {
+                                Quantity = 3,
+                                Color = "Red",
+                                Price = 20.0f,
+                                ProductId = 2,
+                                ProductName = "Product 2",
+                            }
+                        }
+                    }
+                };
+                shoppingCartContext.ShoppingCart.AddRange(shoppingCarts);
+                shoppingCartContext.SaveChanges();
+            }
+
+        }
+    }
+}
