bb67a980b992b520a59e4cdf42e59479f0ff8f9e
Mauricio Souza <mauricio.salso@hotmail.com>
2026-03-07 04:41:41 -0300

    feat: criando o DiscontService e consumindo o serviço de Desconto

--- ShoppingCartGrpc/Services/ShoppingCartService.cs
+++ ShoppingCartGrpc/Services/ShoppingCartService.cs
@@ -14,12 +14,14 @@ namespace ShoppingCartGrpc.Services
     public class ShoppingCartService : ShoppingCartProtoService.ShoppingCartProtoServiceBase
     {
         private readonly ShoppingCartContext _shoppingCartDbContext;
+        private readonly DiscountService _discountService;
         private readonly IMapper _mapper;
         private readonly ILogger<ShoppingCartService> _logger;
 
-        public ShoppingCartService(ShoppingCartContext shoppingCartDbContext, IMapper mapper, ILogger<ShoppingCartService> logger)
+        public ShoppingCartService(ShoppingCartContext shoppingCartDbContext, DiscountService discountService, IMapper mapper, ILogger<ShoppingCartService> logger)
         {
             _shoppingCartDbContext = shoppingCartDbContext ?? throw new ArgumentNullException(nameof(shoppingCartDbContext));
+            _discountService = discountService ?? throw new ArgumentNullException(nameof(discountService));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
@@ -115,8 +117,9 @@ namespace ShoppingCartGrpc.Services
                 else
                 {
                     // grpc call discount service -- check discount and calculate the item last price
-                    float discount = 100;
-                    newAddedCartItem.Price -= discount;
+                    var discount = await _discountService.GetDiscount(requestStream.Current.DiscountCode);
+                    newAddedCartItem.Price -= discount.Amount;
+
                     shoppingCart.Items.Add(newAddedCartItem);
                 }
             }
