1ba51797bac47bfdf13df682412bf5fd46324ca3
Mauricio Souza <mauricio.salso@hotmail.com>
2026-03-07 04:55:19 -0300

    Adicione o cliente gRPC de desconto e a configuração de injeção de dependência (DI) na inicialização.
    Configurado o cliente DiscountProtoService via DI usando as configurações do aplicativo.
    
    Definindo todas as port dos microserviços
    
    Registrado o DiscountService com escopo definido. Inicialização atualizada para usar
    IConfiguration para configuração do serviço. Permite que o ShoppingCartGrpc
    se comunique com o serviço de desconto.

--- ShoppingCartGrpc/Startup.cs
+++ ShoppingCartGrpc/Startup.cs
@@ -1,22 +1,41 @@
 ﻿using AutoMapper;
+using DiscountGrpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ShoppingCartGrpc.Data;
 using ShoppingCartGrpc.Services;
+using System;
 
 namespace ShoppingCartGrpc
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+                options.Address = new Uri(Configuration["GrpcConfigs:DiscountUrl"]));
+
+            // 50001 - Product
+            // 50002 - ShoppingCart
+            // 50003 - Discount
+
+            services.AddScoped<DiscountService>();
 
             services.AddDbContext<ShoppingCartContext>(options =>
                     options.UseInMemoryDatabase("ShoppingCart"));
